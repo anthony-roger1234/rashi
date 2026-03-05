0e9b671f8c4d861578faf9de46dbb7b692380bed
anthony-roger1234 <111300442+anthony-roger1234@users.noreply.github.com>
2026-03-06 05:26:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ const albumTitle = document.getElementById('album-title');
 const mediaContainer = document.getElementById('media-container');
 const backBtn = document.getElementById('back-btn');
 
-// Load albums from albums.json
+// Load all albums from albums.json
 fetch('albums/albums.json')
   .then(res => res.json())
   .then(albumFiles => {
-    albumFiles.forEach(file => {
-      fetch(`albums/${file}`)
-        .then(res => res.json())
-        .then(data => createAlbumCard(data));
-    });
-  });
+    // Fetch all album JSONs in parallel
+    return Promise.all(albumFiles.map(file => fetch(`albums/${file}`).then(res => res.json())));
+  })
+  .then(albums => {
+    albums.forEach(album => createAlbumCard(album));
+  })
+  .catch(err => console.error("Error loading albums:", err));
 
 // Create album card
 function createAlbumCard(album) {
@@ -40,10 +41,19 @@ function showAlbum(album) {
     if (item.type === 'image') {
       element = document.createElement('img');
       element.src = item.src;
+      element.alt = album.title;
     } else if (item.type === 'video') {
       element = document.createElement('iframe');
-      element.src = item.src;
-      element.allow = "autoplay; encrypted-media";
+      // Ensure YouTube videos use embed URL
+      if (item.src.includes("youtube.com/watch")) {
+        element.src = item.src.replace("watch?v=", "embed/");
+      } else {
+        element.src = item.src;
+      }
+      element.width = "300";
+      element.height = "200";
+      element.frameBorder = "0";
+      element.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
       element.allowFullscreen = true;
     }
     mediaContainer.appendChild(element);
